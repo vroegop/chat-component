f85d5a214392715668e7f89db050aaa0878ffade
Randy Vroegop <randy.vroegop@luminis.eu>
2023-05-12 14:06:30 +0200

    Add error handling and engine settings

--- chat-app.js
+++ chat-app.js
@@ -12,6 +12,7 @@ class ChatApp extends LitElement {
             elevenlabsStability: {type: Number},
             elevenlabsSimilarity: {type: Number},
             textToSpeechEnabled: {type: String},
+            engine: {type: String},
             // required properties
             openaiApiKey: {type: String},
             elevenlabsApiKey: {type: String},
@@ -28,17 +29,21 @@ class ChatApp extends LitElement {
         this.elevenlabsStability = 0;
         this.elevenlabsSimilarity = 0;
         this.textToSpeechEnabled = false;
+        this.engine = 'gpt-3.5-turbo';
 
         this.openaiApiKey = localStorage.getItem('openaiApiKey');
         this.elevenlabsApiKey = localStorage.getItem('elevenlabsApiKey');
+
+        this.chatLoading = false;
     }
 
     // Gets called after the properties set via HTML are injected, overriding default values
     connectedCallback() {
         super.connectedCallback();
         this.allPrePrompts = JSON.parse(this.prePrompt.replaceAll('\n', '').replaceAll('  ', ''));
-        this.messages = [ ...this.allPrePrompts.filter(m => m.role !== 'system'), ...this.messages ];
+        this.messages = [...this.allPrePrompts.filter(m => m.role !== 'system'), ...this.messages];
         this.systemPrompts = this.allPrePrompts.filter(m => m.role === 'system');
+        this.addEventListener('new-chat-message', () => this.newChatMessage());
     }
 
     // All CSS goes in here
@@ -61,9 +66,10 @@ class ChatApp extends LitElement {
             flex-direction: column;
             align-content: space-between;
             flex-grow: 1;
+            overflow: auto;
           }
 
-          .messages {
+          #messages {
             list-style: none;
             margin: 0;
             padding: 0;
@@ -77,6 +83,25 @@ class ChatApp extends LitElement {
             border-radius: 8px;
             margin: 8px 0;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+            white-space: pre-line;
+          }
+
+          .message.loading i {
+            font-style: normal;
+            display: inline-block;
+            animation: rotation linear 2s infinite;
+            width: 15px;
+            height: 15px;
+            line-height: 15px;
+          }
+
+          @keyframes rotation {
+            from {
+              transform: rotate(0deg);
+            }
+            to {
+              transform: rotate(360deg);
+            }
           }
 
           .input-container {
@@ -84,7 +109,7 @@ class ChatApp extends LitElement {
             margin-top: 16px;
           }
 
-          .input-container input[type=text] {
+          .input-container textarea {
             flex: 1;
             padding: 8px;
             border-radius: 4px;
@@ -116,17 +141,20 @@ class ChatApp extends LitElement {
     render() {
         return html`
             <div class="container">
-                <ul class="messages">
+                <ul id="messages">
                     ${this.messages.map((message) => html`
-                        <li class="message">${message.content}</li>
+                        <li class="message">${message.role === 'assistant' ? 'DM: ' : message.role === 'system' ? 'Oops: ' : 'You: '}${message.content}</li>
                     `)}
+
+                    ${this.chatLoading ? html`
+                        <li class="message loading"><i>⚛</i></li>` : ''}
                 </ul>
                 <div class="input-container">
-                    <input type="text"
-                           placeholder="Type your message here"
-                           @input="${this.handleInput}"
-                           .value="${this.messageText}"
-                           @keypress="${this.handleKeyPress}"
+                    <textarea type="text"
+                              placeholder="Type your message here"
+                              @input="${this.handleInput}"
+                              .value="${this.messageText}"
+                              @keypress="${this.handleKeyPress}"
                     />
                     <button @click="${this.sendMessage}">Send</button>
                 </div>
@@ -141,9 +169,17 @@ class ChatApp extends LitElement {
 
     // Handles the buttons pressed in the input field
     handleKeyPress(event) {
-        if (event.key === 'Enter') {
+        if (event.key === 'Enter' && event.shiftKey === false) {
             this.sendMessage();
         }
+    }
+
+    // Scrolls to the last added message
+    newChatMessage() {
+        setTimeout(() => {
+            const messageContainer = this.shadowRoot.getElementById('messages');
+            messageContainer.scrollTop = messageContainer.scrollHeight;
+        }, 0);
     }
 
     // Adds the message to the message history, sends the full history to ChatGPT and adds the answer to the history too
@@ -155,9 +191,11 @@ class ChatApp extends LitElement {
             this.messageText = '';
 
             if (this.openaiApiKey) {
+                this.chatLoading = true;
                 const gptResponse = await this.getChatGPTResponse([...this.systemPrompts, ...this.messages]);
                 this.dispatchEvent(new CustomEvent('new-chat-message', {detail: gptResponse}));
                 this.messages = [...this.messages, gptResponse];
+                this.chatLoading = false;
 
                 if (this.elevenlabsApiKey && this.textToSpeechEnabled && this.textToSpeechEnabled !== 'false') {
                     this.textToSpeech(gptResponse.content);
@@ -168,23 +206,31 @@ class ChatApp extends LitElement {
 
     // Connects to ChatGPT to get a chat response
     async getChatGPTResponse(messages) {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${this.openaiApiKey}`
-            },
-            body: JSON.stringify({
-                model: 'gpt-3.5-turbo',
-                messages,
-                max_tokens: this.gptResponseMaxCharacters,
-                temperature: this.gptResponseTemperature,
-                n: 1,
-            })
-        });
+        try {
+            const response = await fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${this.openaiApiKey}`
+                },
+                body: JSON.stringify({
+                    model: this.engine,
+                    messages,
+                    max_tokens: this.gptResponseMaxCharacters,
+                    temperature: this.gptResponseTemperature,
+                    n: 1,
+                })
+            });
 
-        const data = await response.json();
-        return data.choices[0].message;
+            if (!response.ok) {
+                throw new Error('Not ok');
+            }
+
+            const data = await response.json();
+            return data.choices[0].message;
+        } catch (err) {
+            return { role: 'system', content: 'Oh no, the chatbot was unavailable. Please try again.' }
+        }
     }
 
     // Connects to Elevenlabs to create text to speech
@@ -214,4 +260,5 @@ class ChatApp extends LitElement {
         audio.play();
     };
 }
+
 customElements.define('chat-app', ChatApp);
